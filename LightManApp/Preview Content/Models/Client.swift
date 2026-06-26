1a1f33ed32128cded7afff004b17d1b154fd4340
Jareskis <jarekcarlson20@gmail.com>
2026-06-26 09:34:52 -0600

    Created 2 Models
    Created the Models Client and Jobs and connected them with realtionships

---
+++ LightManApp/Preview Content/Models/Client.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+import SwiftData
+import MapKit
+
+@Model
+class Client {
+    var name: String
+    var email: String?
+    var phone : String?
+    var address : String //Figure out how to store address
+    @Relationship(deleteRule: .cascade) var jobs : [Job]
+    
+    init(name: String, email: String?, phone: String?, address: String) {
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.jobs = []
+    }
+    
+}
+
---
+++ LightManApp/Preview Content/Models/Jobs.swift
@@ -0,0 +1,38 @@
+//
+//  Jobs.swift
+//  LightManApp
+//
+//  Created by Jarek Carlson on 6/26/26.
+//
+import SwiftUI
+import Foundation
+
+
+enum JobType: Codable {
+    case install
+    case takedown
+    case repair
+}
+
+enum StatusType: Codable {
+    case notStarted
+    case inProgress
+    case finished
+}
+
+@Model
+class Job {
+    var type : JobType
+    var date : Date
+    var status : StatusType
+    var notes : String?
+    var client : Client?
+    
+    init(type: JobType, date: Date, status: StatusType, notes: String?, client: Client?) {
+        self.type = type
+        self.date = date
+        self.status = status
+        self.notes = notes
+        self.client = client
+    }
+}
